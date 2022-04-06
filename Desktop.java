769811031e894b7210f2dab4c5116345c4d49396
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-04-07 02:54:44 +0300

    Add files via upload

--- Desktop.java
+++ Desktop.java
@@ -1,5 +1,5 @@
 package week3.day1;
-
+//create above 2 class and call all your class methods into the Desktop using single inheritance.
 public class Desktop extends Computer {
 
 	public void desktopSize() {
